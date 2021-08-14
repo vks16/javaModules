35ff435bf91936e3f8ca8c534a7b6b0d8b46b64d
vks16 <vishalsharma16171718@gmail.com>
2021-08-14 23:11:23 +0530

    building with jpackage

--- 2_using_modules/src/main/java/com/agiledeveloper/user/FirstUser.java
+++ 2_using_modules/src/main/java/com/agiledeveloper/user/FirstUser.java
@@ -1,12 +1,20 @@
 package com.agiledeveloper.user;
 
 import com.agiledeveloper.util.*;
+import java.util.*;
 
 public class FirstUser {
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci();
-
-        System.out.println("fib(10) : " + fibonacci.fib(10));
+        System.out.print("Please Enter a Number to get fibonacci number : ");
+        Scanner sc = new Scanner(System.in);
+        
+            if (sc.hasNextInt()) {
+                System.out.println(fibonacci.fib(sc.nextInt()));
+            } else {
+                System.out.println("Please Enter a integer only.");
+            }
+        
 
         useHelper(fibonacci);
     }
@@ -18,16 +26,17 @@ public class FirstUser {
     public static void useHelper(Fibonacci fibonacci) {
         Helper helper = fibonacci.getHelper();
         System.out.println(helper);
-        // com.agiledeveloper.impl.MyHelper method = helper.getClass().getMethod("hidden");
-        //Reflection part
+        // com.agiledeveloper.impl.MyHelper method =
+        // helper.getClass().getMethod("hidden");
+        // Reflection part
         try {
             java.lang.reflect.Method method = helper.getClass().getMethod("hidden");
             // System.out.println(method);
 
             // method.invoke(helper); //Error here
-            //To fix this, we may either place "open"
+            // To fix this, we may either place "open"
 
-        }catch (Exception ex){
+        } catch (Exception ex) {
             System.out.println("Error(): " + ex.getMessage());
         }
     }
--- build.sh
+++ build.sh
@@ -18,16 +18,19 @@ jar -c -f output/mlib/first.jar --main-class com.agiledeveloper.user.FirstUser -
 
 rm -rf output/classes
 
-# java -p output/mlib -m com.agiledeveloper.first
+java -p output/mlib -m com.agiledeveloper.first
 # java -p output/mlib -m com.agiledeveloper.first/com.agiledeveloper.user.FirstUser
 
-javac -p output/mlib -d output/classes `find 3_implied_readability -name *.java`
+# javac -p output/mlib -d output/classes `find 3_implied_readability -name *.java`
 
-jar -c -f output/mlib/second.jar --main-class com.agiledeveloper.second.SecondUser -C output/classes .
+# jar -c -f output/mlib/second.jar --main-class com.agiledeveloper.second.SecondUser -C output/classes .
 
-rm -rf output/classes
+# rm -rf output/classes
 
-java -p output/mlib -m second
+# java -p output/mlib -m second
 
+# packaging
+
+jpackage -n fibonacci -p output/mlib -m com.agiledeveloper.first/com.agiledeveloper.user.FirstUser
 
 
